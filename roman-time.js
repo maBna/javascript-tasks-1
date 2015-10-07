32053ea294522520a13d78a7f4e697d2fa610d84
maBna <554dgdf@gmail.com>
2015-10-07 18:20:37 +0600

    Правка полуфинальная
    Поправил глупую ошибку с выводом N у минут.
    Переназвал локальные переменные.

--- roman-time.js
+++ roman-time.js
@@ -6,17 +6,17 @@ var minutesStr = '';
 
 function HoursToRome(hours)
 {	
-	var a = hours/10; // количество десяток
+	var countTen = hours/10; // количество десяток
 	//var b = ((hours%10)/5); // количество пятерок
-	var c = hours%10; // количество единиц
+	var countOne = hours%10; // количество единиц
 	
 	
-	for (a; a >= 1; a--)
+	for (countTen; countTen >= 1; countTen--)
 	{
-		if (a/4 >= 1)
+		if (countTen/4 >= 1)
 		{
 		hoursStr = hoursStr + 'XL';
-		a = a - 4;
+		countTen = countTen - 4;
 		}
 		else
 		{
@@ -30,49 +30,49 @@ function HoursToRome(hours)
 	}
 	*/
 
-	while (c >= 1)
+	while (countOne >= 1)
 	{
-		if (c/9 >= 1) //9 по фен-шую
+		if (countOne/9 >= 1) //9 по фен-шую
 		{
 		hoursStr = hoursStr + 'IX';
-		c = c - 9;
+		countOne = countOne - 9;
 		}
-		else if (c/5 >= 1) //5 по фен-шую
+		else if (countOne/5 >= 1) //5 по фен-шую
 		{
 		hoursStr = hoursStr + 'V';
-		c = c - 5;
+		countOne = countOne - 5;
 		}
-		else if (c/4 >= 1) //4 по фен-шую
+		else if (countOne/4 >= 1) //4 по фен-шую
 		{
 		hoursStr = hoursStr + 'IV';
-		c = c - 4;
+		countOne = countOne - 4;
 		}
 		else		
 		{
 		hoursStr = hoursStr + 'I';
-		c--;
+		countOne--;
 		}
 	}
 }
 
 function MinutesToRome(minutes)
 
-{	var d = minutes/50; //количество 50
-	var a = (minutes%50)/10; // количество 10
+{	var countFifty = minutes/50; //количество 50
+	var countTen = (minutes%50)/10; // количество 10
 	//var b = ((minutes%10)/5); // количество 5
-	var c = minutes%10; // количество 1
+	var countOne = minutes%10; // количество 1
 	
-	for (d; d >= 1; d--)
+	for (countFifty; countFifty >= 1; countFifty--)
 	{
 		minutesStr = minutesStr + 'L';
 	}
 	
-	for (a; a >= 1; a--)
+	for (countTen; countTen >= 1; countTen--)
 	{
-		if (a/4 >= 1)
+		if (countTen/4 >= 1)
 		{
 		minutesStr = minutesStr + 'XL';
-		a = a - 4;
+		countTen = countTen - 4;
 		}
 		else
 		{
@@ -85,27 +85,27 @@ function MinutesToRome(minutes)
 		minutesStr = minutesStr + 'V';
 	}*/
 	
-	while (c >= 1)
+	while (countOne >= 1)
 	{
-		if (c/9 >= 1) //9 по фен-шую
+		if (countOne/9 >= 1) //9 по фен-шую
 		{
 		minutesStr = minutesStr + 'IX';
-		c = c - 9;
+		countOne = countOne - 9;
 		}
-		else if (c/5 >= 1) //5 по фен-шую
+		else if (countOne/5 >= 1) //5 по фен-шую
 		{
 		minutesStr = minutesStr + 'V';
-		c = c - 5;
+		countOne = countOne - 5;
 		}
-		else if (c/4 >= 1) //4 по фен-шую
+		else if (countOne/4 >= 1) //4 по фен-шую
 		{
 		minutesStr = minutesStr + 'IV';
-		c = c - 4;
+		countOne = countOne - 4;
 		}
 		else		
 		{
 		minutesStr = minutesStr + 'I';
-		c--;
+		countOne--;
 		}
 	}
 }
@@ -122,10 +122,9 @@ if (hours <= 23 && hours >= 0)
 		}
 		else if (minutes == 0)
 		{
-		
+			minutesStr = "N";
 			HoursToRome(hours);
 			console.log(hoursStr + ':' + minutesStr);
-			minutesStr = "N";
 		}
 		else 
 		{
